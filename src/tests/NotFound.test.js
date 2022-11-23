9cfba7748a1e8a26b8373de6d3813cd94480dbb0
NettoJM <nettojm86@gmail.com>
2022-11-23 18:10:45 -0300

    testing the NotFound component

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import { NotFound } from '../pages';
+
+describe('Testes do componente <NotFound.js />', () => {
+  it('testa se a página contém um heading h2 com o texto Page requested not found', () => {
+    render(<NotFound />);
+
+    const notFoundHeading = screen.getByRole('heading', { name: 'Page requested not found' });
+    expect(notFoundHeading).toBeInTheDocument();
+  });
+
+  it('testa se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    render(<NotFound />);
+
+    const URL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    const sadPikachuImage = screen.getByRole('img');
+    expect(sadPikachuImage).toHaveAttribute('src', URL);
+    expect(sadPikachuImage).toHaveAttribute('alt', 'Pikachu crying because the page requested was not found');
+    expect(sadPikachuImage).toBeInTheDocument();
+  });
+});
