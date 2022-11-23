0976f14b026ce2f479e6608e2563b07d966e1bf8
NettoJM <nettojm86@gmail.com>
2022-11-23 20:35:59 -0300

    testing the FavoritePokemon component

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,45 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import { FavoritePokemon } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testes do componente <FavoritePokemon.js />', () => {
+  it('testa se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos', () => {
+    render(<FavoritePokemon />);
+
+    const noFavoriteMessage = screen.getByText('No favorite Pokémon found');
+    expect(noFavoriteMessage).toBeInTheDocument();
+  });
+
+  it('Teste se são exibidos todos os cards de Pokémon favoritados', () => {
+    renderWithRouter(<App />);
+
+    const pikachu = screen.getByText('Pikachu');
+    expect(pikachu).toBeInTheDocument();
+
+    userEvent.click(screen.getByRole('link', { name: 'More details' }));
+    userEvent.click(screen.getByRole('checkbox', { name: 'Pokémon favoritado?' }));
+
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    userEvent.click(homeLink);
+
+    const nextButton = screen.getByRole('button', { name: 'Próximo Pokémon' });
+    userEvent.click(nextButton);
+    userEvent.click(nextButton);
+
+    const caterpie = screen.getByText('Caterpie');
+    expect(caterpie).toBeInTheDocument();
+
+    userEvent.click(screen.getByRole('link', { name: 'More details' }));
+    userEvent.click(screen.getByRole('checkbox', { name: 'Pokémon favoritado?' }));
+
+    const favoritesLink = screen.getByRole('link', { name: 'Favorite Pokémon' });
+    userEvent.click(favoritesLink);
+
+    const favoritePokemon = screen.getAllByTestId('pokemon-name');
+    expect(favoritePokemon[0]).toHaveTextContent('Pikachu');
+    expect(favoritePokemon[1]).toHaveTextContent('Caterpie');
+  });
+});
