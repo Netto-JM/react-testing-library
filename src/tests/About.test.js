3cbb4dbdd8ce9e9ccddee3ec7b5132148441c9ec
NettoJM <nettojm86@gmail.com>
2022-11-23 16:26:11 -0300

    testing the About component

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import { About } from '../pages';
+
+describe('Testes o componente <About.js />', () => {
+  it('testa se a página contém um heading h2 com o texto About Pokédex', () => {
+    render(<About />);
+
+    const aboutHeading = screen.getByRole('heading', { name: 'About Pokédex' });
+    expect(aboutHeading).toBeInTheDocument();
+  });
+
+  it('testa se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    render(<About />);
+
+    const firstParagraph = screen.getByText('This application simulates a Pokédex, a digital encyclopedia containing all Pokémon');
+    expect(firstParagraph).toBeInTheDocument();
+
+    const secondParagraph = screen.getByText('One can filter Pokémon by type, and see more details for each one of them');
+    expect(secondParagraph).toBeInTheDocument();
+  });
+
+  it('testa se a página contém a seguinte imagem de uma Pokédex: https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png', () => {
+    render(<About />);
+
+    const pokedexImage = screen.getByRole('img');
+    expect(pokedexImage).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    expect(pokedexImage).toHaveAttribute('alt', 'Pokédex');
+    expect(pokedexImage).toBeInTheDocument();
+  });
+});
