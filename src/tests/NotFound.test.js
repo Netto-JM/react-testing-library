cad43282375e227519a20077f71decdd19c578d9
NettoJM <nettojm86@gmail.com>
2022-11-26 16:18:22 -0300

    testing the level of the heading, testing the name of the image, removing redundant alt test

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -6,7 +6,7 @@ describe('Testes do componente <NotFound.js />', () => {
   it('testa se a página contém um heading h2 com o texto Page requested not found', () => {
     render(<NotFound />);
 
-    const notFoundHeading = screen.getByRole('heading', { name: 'Page requested not found' });
+    const notFoundHeading = screen.getByRole('heading', { name: 'Page requested not found', level: 2 });
     expect(notFoundHeading).toBeInTheDocument();
   });
 
@@ -15,9 +15,8 @@ describe('Testes do componente <NotFound.js />', () => {
 
     const URL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
 
-    const sadPikachuImage = screen.getByRole('img');
+    const sadPikachuImage = screen.getByRole('img', { name: /pikachu crying/i });
     expect(sadPikachuImage).toHaveAttribute('src', URL);
-    expect(sadPikachuImage).toHaveAttribute('alt', 'Pikachu crying because the page requested was not found');
     expect(sadPikachuImage).toBeInTheDocument();
   });
 });
